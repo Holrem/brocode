524d7a9ce04cc6794d199c82d34d01894ba02ca8
Holrem <c.t.grinnell@gmail.com>
2024-08-20 10:13:59 -0500

    Added typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,26 @@
+# Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+name = "Holrem"
+age = 37
+gpa = 3.0
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(is_student))
+
+name = bool(name)
+print(name)
+
+age = float(age)
+print(age)
+
+gpa = int(gpa)
+print(gpa)
+
+is_student = str(is_student)
+print(is_student)
+
+
+
