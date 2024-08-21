b8cd1f6ace849c7561852f3d7fa6d029a2c91ea2
Holrem <c.t.grinnell@gmail.com>
2024-08-21 11:54:58 -0500

    Added madlibs game

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+# Madlibs game
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (a person, place, or thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing' (an action): ")
+adjective3 = input("Enter an adjective (description): ")
+
+
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
