e2f84314e2a8d0366e037d7cf4c593bb414c480b
Holrem <c.t.grinnell@gmail.com>
2024-08-21 12:20:43 -0500

    Added math operators and exercise 2

---
+++ math.py
@@ -0,0 +1,40 @@
+# Operators
+# friends = 0
+# friends = friends + 1
+# friends += 1
+# friends = friends - 2
+# friends -= 2
+# friends = friends * 3
+# friends *= 3
+# friends = friends / 2
+# friends /= 2
+# friends = friends ** 2
+# friends **= 2
+# remainder = friends % 2
+# print(friends)
+
+# x = 3.14
+# y = 4
+# z = 5
+# result = round(x)
+# print(result)
+# result = abs(y)
+# print(result)
+# result = pow(4,3)
+# print(result)
+# result = max(x, y, z)
+# print(result)
+# result = min(x, y, z)
+# print(result)
+
+
+#import math
+
+#x = 9
+
+#result = math.sqrt(x)
+#print(result)
+#result = math.ceil(x)
+#print(result)
+#result = math.floor(x)
+#print(result)
